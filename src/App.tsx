ef25e17032d6b3878b4c5eff9b76262c5406304d
ElQVendePan <danielmenses282@gmail.com>
2025-09-29 15:42:23 -0500

    added routes for basic nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import Modal from './components/Modal'
+import { useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import { useState, useEffect } from 'react'
 
 const App = () => {
+  const [currentPath, setCurrentPath] = useState('/')
+  const location = useLocation()
+
+  useEffect(() => {
+    setCurrentPath(location.pathname)
+  }, [location.pathname]) // <- solo se ejecuta cuando cambia la ruta
+
   return (
     <>
       <div>
         <h1 className='text-3xl font-bold'>Hello, world!</h1>
         <p>lorem ipsum dolor sit amet</p>
       </div>
-      <Navbar></Navbar>
-      <Modal></Modal>
+      <Navbar currentPath={currentPath} />
     </>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,11 @@ import { FaSearch } from 'react-icons/fa'
 import { HiMegaphone } from 'react-icons/hi2'
 import { IoPersonCircleSharp } from 'react-icons/io5'
 import { RiHome6Fill } from 'react-icons/ri'
+import { Link } from 'react-router-dom'
 
 const NavbarItem = ({ icon, active }: { icon: React.ReactNode, active?: boolean }) => {
   return (
-    <div className={`items-center justify-center flex p-2 rounded-full ${active ? 'bg-primary text-white opacity-100' : 'opacity-30'}`}>
+    <div className={`items-center justify-center flex p-2 rounded-full ${active ? 'bg-primary text-white opacity-100' : 'opacity-30'} duration-200`}>
       <div className='text-2xl'>
         {icon}
       </div>
@@ -13,14 +14,22 @@ const NavbarItem = ({ icon, active }: { icon: React.ReactNode, active?: boolean 
   )
 }
 
-const Navbar = () => {
+const Navbar = ({ currentPath }: { currentPath: string }) => {
   return (
     <div className='bg-white px-4 pb-2 fixed bottom-0 w-full text-center'>
       <div className='grid grid-cols-4 gap-4'>
-        <NavbarItem icon={<RiHome6Fill />} />
-        <NavbarItem icon={<FaSearch />} />
-        <NavbarItem active icon={<HiMegaphone />} />
-        <NavbarItem icon={<IoPersonCircleSharp />} />
+        <Link to='/'>
+          <NavbarItem icon={<RiHome6Fill />} active={currentPath === '/'} />
+        </Link>
+        <Link to='/search'>
+          <NavbarItem icon={<FaSearch />} active={currentPath === '/search'} />
+        </Link>
+        <Link to='/notifications'>
+          <NavbarItem icon={<HiMegaphone />} active={currentPath === '/notifications'} />
+        </Link>
+        <Link to='/profile'>
+          <NavbarItem icon={<IoPersonCircleSharp />} active={currentPath === '/profile'} />
+        </Link>
       </div>
     </div>
   )
